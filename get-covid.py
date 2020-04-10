1c6f1186fe58de6eb9407979165c11e8b8046db8
DaveMorais <dmorais@ccs.usherbrooke.ca>
2020-04-10 18:42:41 -0400

    doc

--- get-covid.py
+++ get-covid.py
@@ -5,7 +5,10 @@ import os
 
 
 def main():
-    
+    '''
+        Main function
+    '''
+
     url = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats"
 
     querystring = {"country": args.country}
